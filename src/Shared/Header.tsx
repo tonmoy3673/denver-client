7196bf21ea641f54a7ce9b396fd528c5dc3b039f
tonmoy <tonmoy3673@gmail.com>
2025-01-22 21:37:21 +0600

    :bulb: create header footer page

---
+++ src/Shared/Header.tsx
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <div>
+      <h2>This is Header page</h2>
+    </div>
+  );
+};
+
+export default Header;
---
+++ src/pages/Services/Services.tsx
@@ -0,0 +1,9 @@
+const Services = () => {
+  return (
+    <div>
+      <h2>This is service page</h2>
+    </div>
+  );
+};
+
+export default Services;

963ff3ed4b2a856030e5c3407da2b4bd20f691cb
Md Alamin <alaminduc516@gmail.com>
2025-01-22 23:09:16 +0600

    :tada: main layout route created

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Main from "./pages/Main/Main";
+import Home from "./pages/Home/Home";
 
 function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<Main />} />
+        <Route path="/" element={<Main />}>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/services" element={<Home/>}/>
+        </Route>
       </Routes>
     </>
   );

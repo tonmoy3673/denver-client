c59e8969da045f319ff07aa8bb10e7bf852d2b1c
tonmoy <tonmoy3673@gmail.com>
2025-01-24 23:29:19 +0600

    :bug: fixed home page

--- src/pages/Home/Community.tsx
+++ src/pages/Home/Community.tsx
@@ -4,19 +4,21 @@ const Community = () => {
       {/* ========== Community Main Container ========== */}
       <div className="bg-[#12778C] container mx-auto rounded-[48px] grid grid-cols-7 items-center justify-between max-h-[400px] ">
         {/* ======== Community Icon ======== */}
-        <div className="col-span-4 bg-red-600">
+        <div className="col-span-4 bg-green-500">
           <div className="w-[414px] h-[400px] bg-[url('/src/assets/icons/communityIcon.svg')] bg-cover bg-center flex flex-col items-center justify-center">
-            <div className="w-[537px] flex items-center justify-end bg-amber-500 ">
-              <h2 className="text-start font-libre text-xl md:text-2xl lg:text-[34px] tracking-[-2.88px]  text-white lg:leading-[140%] font-bold">
+            <div className="w-[537px]  flex items-center justify-end bg-amber-500 ">
+              <h2 className="text-start font-libre text-xl md:text-2xl lg:text-[34px] tracking-[-1.88px]  text-white lg:leading-[140%] font-bold">
                 What are you waiting for? <br />
                 Sign up and join the rest of <br /> the Colorado dog community
               </h2>
             </div>
-            <div className="mt-5 lg:mt-7 flex flex-col md:flex-row items-center space-y-3 md:space-y-0 space-x-0 md:space-x-6 lg:space-x-10">
+            <div className="mt-5 lg:mt-7 flex flex-col md:flex-row items-center space-y-3 md:space-y-0 space-x-0 md:space-x-6 lg:space-x-6 ml-24">
               <button className="lg:w-[273px] text-white font-libre font-semibold h-[50px] bg-[#FF8414] rounded-md">
                 Sign Up! - It’s FREE!
               </button>
-              <img src="/src/assets/icons/Vector.svg" />
+              <div className="bg-red-600">
+                <img src="/src/assets/icons/Vector.svg" />
+              </div>
             </div>
           </div>
         </div>
